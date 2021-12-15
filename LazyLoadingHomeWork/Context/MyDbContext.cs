5c792f4e38e979bd45d4d90222bc1d9ec32a3ff7
nicat ceferli <mega.ceferli@gmail.com>
2021-12-15 23:15:20 +0400

    Add project files.

---
+++ LazyLoadingHomeWork/Context/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using LazyLoadingHomeWork.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoadingHomeWork.Context
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-E5M7S6J;Initial Catalog=TaskEntityHomeWork2;Integrated Security=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ LazyLoadingHomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using LazyLoadingHomeWork.Context;
+using LazyLoadingHomeWork.Models;
+using System;
+using System.Linq;
+
+namespace LazyLoadingHomeWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var temp = new MyDbContext())
+            {
+                var temp2=temp.Categories.FirstOrDefault();
+
+                foreach (var item in temp2.Products.ToList())
+                    Console.WriteLine(item.Name);
+                //burda eager loading olsaydi biz categories icerisindeki Productlara cata bilmezdik
+                //ama burda lazy loading dir deye biz productlara cata bilerik
+            }
+        }
+    }
+}
